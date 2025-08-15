3775e785214c9329d11378336809efc20c1bed22
Seu Nome <mrssxzica@gmail.com>
2025-08-15 00:30:08 -0300

    first commit

---
+++ MEUPROJETO/PROJETOFINALTHEUS/index.html/inicio.js
@@ -0,0 +1,4 @@
+// Exemplo de função para alertar uma mensagem
+function showMessage() {
+    alert("Obrigado por visitar meu portfólio!");
+}
---
+++ MEUPROJETO/formulario_db.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 25/11/2024 às 01:21
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `formulario_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `contatos`
+--
+
+CREATE TABLE `contatos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `celular` varchar(20) NOT NULL,
+  `mensagem` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `contatos`
+--
+
+INSERT INTO `contatos` (`id`, `nome`, `email`, `celular`, `mensagem`) VALUES
+(1, 'Matheus Oliveira', 'mathgta368@gmail.com', '(11) 93233-5737', 'Sou lindo'),
+(2, 'Otávio Oliveira', 'otaviooliveira@gmail.com', '11982019040', 'Eu sou feio'),
+(3, 'Tais Regina', 'taisoliveirarm@gmail.com', '93231342233', 'Eu sou linda'),
+(4, 'Guilherme Vital Schultz', 'guilhermevital@gmail.com', '1193232808', 'Eu sou negão'),
+(5, 'Luiz Fellipe Modesto', 'lipequeiroz@gmail.com', '11932456423', 'Eu tenho probleminhas');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `contatos`
+--
+ALTER TABLE `contatos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `contatos`
+--
+ALTER TABLE `contatos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
